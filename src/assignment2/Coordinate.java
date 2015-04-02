34422902685eb61edddee45ab62105df130fa2f2
yuxiaoli <yuxiaoli@usc.edu>
2015-04-02 12:27:22 -0700

    a2 class structure completed

--- src/assignment2/Coordinate.java
+++ src/assignment2/Coordinate.java
@@ -1,5 +1,18 @@
 package assignment2;
 
-public class Coordinate {
-
+public abstract class Coordinate {
+	private double value1, value2;
+	public Coordinate(double val1, double val2) {
+		value1 = val1;
+		value2 = val2;
+	}
+	public abstract double getDistance(Coordinate c);
+	public abstract double getSlopeOfLine(Coordinate c);
+	protected double getValue1() {
+		return value1;
+	}
+	protected double getValue2() {
+		return value2;
+	}
+	
 }
--- src/assignment2/PolarCoordinate.java
+++ src/assignment2/PolarCoordinate.java
@@ -1,5 +1,22 @@
 package assignment2;
 
-public class PolarCoordinate {
+public class PolarCoordinate extends Coordinate {
+
+	public PolarCoordinate(double val1, double val2) {
+		super(val1, val2);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getDistance(Coordinate c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getSlopeOfLine(Coordinate c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
