5fe439908dd74466f9dc23d125becdfb83f849aa
Yuxiao Li <yuxiaoli@usc.edu>
2015-04-05 18:40:21 -0700

    a2 main func structure 880% completed

--- src/assignment2/Coordinate.java
+++ src/assignment2/Coordinate.java
@@ -29,8 +29,10 @@ public abstract class Coordinate {
 		InputStreamReader istream = new InputStreamReader(System.in);
 		BufferedReader bufRead = new BufferedReader(istream);
 		
+		boolean limbo = true;
+		
 		try {
-			while (true) {
+			while (limbo) {
 				
 				System.out.println("[polar] Ploar Coordinates (r, theta)");
 				System.out.println("[cartesian] Cartesian Coordinates (x, y)");
@@ -40,15 +42,111 @@ public abstract class Coordinate {
 				System.out.println("What type of coordinates?");
 				
 				if (bufRead.readLine().equals("polar")) {
-					break;
+					while (true) {
+						System.out.println("[degrees] Degrees");
+						System.out.println("[radians] Radians");
+						
+						if (bufRead.readLine().equals("degrees")) {
+							System.out.println("Coordinate 1 - Please enter the radius: ");
+							double r1 = Integer.parseInt(bufRead.readLine());
+							System.out.println("Coordinate 1 - Please enter theta (degrees): ");
+							double t1 = Integer.parseInt(bufRead.readLine());
+							System.out.println("Coordinate 2 - Please enter the radius: ");
+							double r2 = Integer.parseInt(bufRead.readLine());
+							System.out.println("Coordinate 2 - Please enter theta (degrees): ");
+							double t2 = Integer.parseInt(bufRead.readLine());
+							
+							while (true) {
+								System.out.println("[convert] Convert to Cartesian coordinates");
+								System.out.println("distance] Find the distance between the two points");
+								System.out.println("[slope] Find the slope of the line between the points");
+								System.out.println("[equaltion] Find the equation of the line between the points");
+								System.out.println("[menu] Return to main menu");
+								
+								System.out.println("What would you like to do?");
+								if (bufRead.readLine().equals("convert")) {
+									break;
+								}
+								else if (bufRead.readLine().equals("distance")) {
+									break;
+								}
+								else if (bufRead.readLine().equals("slope")) {
+									break;
+								}
+								else if (bufRead.readLine().equals("equation")) {
+									break;
+								}
+								else if (bufRead.readLine().equals("menu")) {
+									break;
+								}
+								else {
+									System.out.println("Please enter one of the options provided.");
+								}
+							}
+							
+							break;
+						}
+						else if (bufRead.readLine().equals("radians")) {
+							break;
+						}
+						else {
+							System.out.println("Please enter one of the options provided.");
+						}
+					}
+					//break;
 				}
 				else if (bufRead.readLine().equals("cartesian")) {
-					break;
+					while (true) {
+						System.out.println("Coordinate 1 - Please enter x: ");
+						double x1 = Integer.parseInt(bufRead.readLine());
+						System.out.println("Coordinate 1 - Please enter y: ");
+						double y1 = Integer.parseInt(bufRead.readLine());
+						System.out.println("Coordinate 2 - Please enter x: ");
+						double x2 = Integer.parseInt(bufRead.readLine());
+						System.out.println("Coordinate 2 - Please enter y: ");
+						double y2 = Integer.parseInt(bufRead.readLine());
+						
+						while (true) {
+							System.out.println("[convert] Convert to Cartesian coordinates");
+							System.out.println("distance] Find the distance between the two points");
+							System.out.println("[slope] Find the slope of the line between the points");
+							System.out.println("[equaltion] Find the equation of the line between the points");
+							System.out.println("[menu] Return to main menu");
+							
+							System.out.println("What would you like to do?");
+							if (bufRead.readLine().equals("convert")) {
+								break;
+							}
+							else if (bufRead.readLine().equals("distance")) {
+								break;
+							}
+							else if (bufRead.readLine().equals("slope")) {
+								break;
+							}
+							else if (bufRead.readLine().equals("equation")) {
+								break;
+							}
+							else if (bufRead.readLine().equals("menu")) {
+								break;
+							}
+							else {
+								System.out.println("Please enter one of the options provided.");
+							}
+						}
+					}
+					//break;
 				}
 				else if (bufRead.readLine().equals("file")) {
-					break;
+					System.out.println("Enter the input filename: ");
+					String inputFilename = bufRead.readLine();
+					System.out.println("Enter the output filename: ");
+					String outputFilename = bufRead.readLine();
+					
+					System.out.println("File was parsed and output generated.");
+					//break;
 				}
 				else if (bufRead.readLine().equals("exit")) {
+					System.out.println("Thank you for using my program!");
 					break;
 				}
 				else {
