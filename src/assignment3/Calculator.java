f936dbd3987c1accb4b2682d53d3f4ed92eea0e7
Yuxiao Li <yuxiaoli@usc.edu>
2015-04-23 23:05:36 -0700

    a3 started

---
+++ src/assignment3/Calculator.java
@@ -0,0 +1,76 @@
+package assignment3;
+
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Calculator extends JFrame {
+	public Calculator() {
+		super("Calculator");
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		JButton invButton = new JButton("Inv");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		southPanel.add(invButton, gbc);
+		
+		JButton lnButton = new JButton("ln");
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		southPanel.add(lnButton, gbc);
+		
+		JButton leftParenthesisButton = new JButton("(");
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		southPanel.add(leftParenthesisButton, gbc);
+		
+		JButton rightParenthesisButton = new JButton(")");
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		southPanel.add(rightParenthesisButton, gbc);
+		
+		JButton deleteButton = new JButton("←");
+		gbc.gridx = 4;
+		gbc.gridy = 0;
+		southPanel.add(deleteButton, gbc);
+		
+		JButton clearAllButton = new JButton("CE");
+		gbc.gridx = 5;
+		gbc.gridy = 0;
+		southPanel.add(clearAllButton, gbc);
+		
+		JButton clearButton = new JButton("C");
+		gbc.gridx = 6;
+		gbc.gridy = 0;
+		southPanel.add(clearButton, gbc);
+		
+		JButton negateButton = new JButton("±");
+		gbc.gridx = 7;
+		gbc.gridy = 0;
+		southPanel.add(negateButton, gbc);
+		
+		JButton squareRootButton = new JButton("√");
+		gbc.gridx = 8;
+		gbc.gridy = 0;
+		southPanel.add(squareRootButton, gbc);
+		
+		add(southPanel, BorderLayout.SOUTH);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setSize(400, 400);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String [] args) {
+		new Calculator();
+	}
+}
