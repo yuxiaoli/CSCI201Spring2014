3ed1767ed1c1a407e8384af426b06f7e0420be0e
yuxiaoli <yuxiaoli@usc.edu>
2015-05-05 20:07:14 -0700

    a3 in progress

--- src/assignment3/Calculator.java
+++ src/assignment3/Calculator.java
@@ -36,7 +36,7 @@ public class Calculator extends JFrame {
 		gbc.gridy = 0;
 		southPanel.add(rightParenthesisButton, gbc);
 		
-		JButton deleteButton = new JButton("←");
+		JButton deleteButton = new JButton("â†�");
 		gbc.gridx = 4;
 		gbc.gridy = 0;
 		southPanel.add(deleteButton, gbc);
@@ -60,6 +60,54 @@ public class Calculator extends JFrame {
 		gbc.gridx = 8;
 		gbc.gridy = 0;
 		southPanel.add(squareRootButton, gbc);
+		
+		
+		JButton sinhButton = new JButton("sinh");
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		southPanel.add(sinhButton, gbc);
+		
+		JButton sinButton = new JButton("sin");
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		southPanel.add(sinButton, gbc);
+		
+		JButton squareButton = new JButton("x");
+		gbc.gridx = 2;
+		gbc.gridy = 1;
+		southPanel.add(squareButton, gbc);
+		
+		JButton rightParenthesisButton = new JButton(")");
+		gbc.gridx = 3;
+		gbc.gridy = 0;
+		southPanel.add(rightParenthesisButton, gbc);
+		
+		JButton deleteButton = new JButton("â†�");
+		gbc.gridx = 4;
+		gbc.gridy = 0;
+		southPanel.add(deleteButton, gbc);
+		
+		JButton clearAllButton = new JButton("CE");
+		gbc.gridx = 5;
+		gbc.gridy = 0;
+		southPanel.add(clearAllButton, gbc);
+		
+		JButton clearButton = new JButton("C");
+		gbc.gridx = 6;
+		gbc.gridy = 0;
+		southPanel.add(clearButton, gbc);
+		
+		JButton negateButton = new JButton("±");
+		gbc.gridx = 7;
+		gbc.gridy = 0;
+		southPanel.add(negateButton, gbc);
+		
+		JButton squareRootButton = new JButton("√");
+		gbc.gridx = 8;
+		gbc.gridy = 0;
+		southPanel.add(squareRootButton, gbc);
+		
+		
 		
 		add(southPanel, BorderLayout.SOUTH);
 		
